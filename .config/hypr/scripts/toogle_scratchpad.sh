71351f4fa04e66919688b46d54ae9e9dc97d967a
Gyan Hari Dahal <dgyanhari@gmail.com>
2025-12-28 18:27:53 +0545

    Removed old files,made a anime focused wallpaper

--- .config/hypr/scripts/toogle_scratchpad.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-if hyprctl -j clients | grep -q '"title": "scratchpad"'; then
-    hyprctl dispatch togglespecialworkspace scratchpad
-else
-    kitty --title scratchpad &
-    sleep 0.15
-    hyprctl dispatch togglespecialworkspace scratchpad
-fi
